ec364fe042a6c132812a15d58e5c285f8e9abed9
cole-maxwell1 <cole.n.maxwell99@gmail.com>
2022-02-07 09:38:08 -0600

    Add testGetAllToDos

---
+++ server/src/test/java/umm3601/todos/ToDosControllerTest.java
@@ -0,0 +1,49 @@
+package umm3601.todos;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+
+import java.io.IOException;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
+
+import io.javalin.http.Context;
+import umm3601.Server;
+
+public class ToDosControllerTest {
+
+  private Context ctx = mock(Context.class);
+
+  private ToDosController userController;
+  private static ToDosDatabase db;
+
+  @BeforeEach
+  public void setUp() throws IOException {
+    ctx.clearCookieStore();
+
+    db = new ToDosDatabase(Server.TODOS_DATA_FILE);
+    userController = new ToDosController(db);
+  }
+
+    @Test
+    void testGetAllToDos() {
+      // Call the method on the mock context, which doesn't
+    // include any filters, so we should get all the users
+    // back.
+    userController.getAllToDos(ctx);
+
+    // Confirm that `json` was called with all the users.
+    ArgumentCaptor<ToDos[]> argument = ArgumentCaptor.forClass(ToDos[].class);
+    verify(ctx).json(argument.capture());
+    assertEquals(db.size(), argument.getValue().length);
+
+    }
+
+    @Test
+    void testGetToDoByID() {
+
+    }
+}
