77cfba7fe5d2fe1738be372270c32a11abad2948
Cole Maxwell <cole.n.maxwell99@gmail.com>
2022-02-07 16:47:38 -0600

    Test /api/users/{id} endpoint

--- server/src/test/java/umm3601/todos/ToDosControllerTest.java
+++ server/src/test/java/umm3601/todos/ToDosControllerTest.java
@@ -3,14 +3,17 @@ package umm3601.todos;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 import java.io.IOException;
+import java.util.HashMap;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.ArgumentCaptor;
 
 import io.javalin.http.Context;
+import io.javalin.http.HttpCode;
 import umm3601.Server;
 
 public class ToDosControllerTest {
@@ -28,9 +31,9 @@ public class ToDosControllerTest {
     userController = new ToDosController(db);
   }
 
-    @Test
-    void testGetAllToDos() {
-      // Call the method on the mock context, which doesn't
+  @Test
+  void testGetAllToDos() {
+    // Call the method on the mock context, which doesn't
     // include any filters, so we should get all the users
     // back.
     userController.getAllToDos(ctx);
@@ -40,10 +43,19 @@ public class ToDosControllerTest {
     verify(ctx).json(argument.capture());
     assertEquals(db.size(), argument.getValue().length);
 
-    }
+  }
 
-    @Test
-    void testGetToDoByID() {
+  @Test
+  void testGetToDoByID() throws IOException {
+    String id = "5889598585bda42fb8388ba1";
+    ToDos todo = db.getToDos(id);
 
-    }
+    when(ctx.pathParam("id")).thenReturn(id);
+
+    userController.getToDoByID(ctx);
+
+    verify(ctx).json(todo);
+    verify(ctx).status(HttpCode.OK);
+
+  }
 }
