ecd8afc6a7cfc45b2b32bf3d177c3e7ab3ebee4a
cole-maxwell1 <cole.n.maxwell99@gmail.com>
2022-02-07 09:38:51 -0600

    Add getToDoByID and getAllToDos methods

--- server/src/main/java/umm3601/todos/ToDosController.java
+++ server/src/main/java/umm3601/todos/ToDosController.java
@@ -1,4 +1,51 @@
 package umm3601.todos;
+
+import io.javalin.http.Context;
+import io.javalin.http.HttpCode;
+import io.javalin.http.NotFoundResponse;
+
 public class ToDosController {
+
+  private ToDosDatabase database;
+
+  /**
+   * Construct a controller for To-Dos.
+   * <p>
+   * This loads the "database" of To-Dos info from a JSON file and stores that
+   * internally so that (subsets of) To-Dos can be returned in response to
+   * requests.
+   *
+   * @param database the `Database` containing To-Dos data
+   */
+  public ToDosController(ToDosDatabase database) {
+    this.database = database;
+  }
+
+
+  /**
+   * Get the single To-Do specified by the `id` parameter in the request.
+   *
+   * @param ctx a Javalin HTTP context
+   */
+  public void getToDoByID(Context ctx) {
+    String id = ctx.pathParam("id");
+    ToDos todo = database.getToDos(id);
+    if (todo != null) {
+      ctx.json(todo);
+      ctx.status(HttpCode.OK);
+    } else {
+      throw new NotFoundResponse("No To-Do with id " + id + " was found.");
+    }
+  }
+
+    /**
+   * Get a JSON response with a list of all the To-Dos in the "database".
+   *
+   * @param ctx a Javalin HTTP context
+   */
+  public void getAllToDos(Context ctx) {
+    ToDos[] todos = database.listToDos(ctx.queryParamMap());
+    ctx.json(todos);
+  }
 
 }
